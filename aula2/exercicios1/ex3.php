9e233d77a9b8943e81b57aa7fffa19196f79a9fc
Daniel Medeiros <134729425+danxzxz@users.noreply.github.com>
2024-02-16 15:40:42 -0300

    Delete aula2/exercicios1 directory

--- aula2/exercicios1/ex3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $nome = readline("Informe seu nome: ");
-    $peso = readline("\ninforme seu peso(em kg): ");
-    $alt = readline("\ninforme sua altura(em Metros): ");
-
-    $imc = $peso / ($alt * $alt);
-
-    echo ("Nome: $nome\n" . "Peso: $peso\n" . "Altura: $alt\n" . "IMC: $imc\n");
