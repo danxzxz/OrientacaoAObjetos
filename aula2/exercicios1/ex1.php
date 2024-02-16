17f1323b15f7868301c4fd075447e38ced266c13
Daniel Medeiros <134729425+danxzxz@users.noreply.github.com>
2024-02-16 15:39:44 -0300

    Add files via upload

---
+++ aula2/exercicios1/ex1.php
@@ -0,0 +1,14 @@
+<?php
+
+for($i=0; $i<=10; $i++){
+$num = readline("Informe um número: ");
+
+if ($num >= 0)
+    echo $num . " é positivo\n";
+else 
+    echo $num . " é negativo\n";
+}
+
+
+
+
---
+++ aula2/exercicios2/ex1.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$num = readline("Informe um número: ");
+
+if ($num >= 0)
+    echo "Numéro informado: $num" . "\nO dobro desse número é: " . $num * 2;
+else //negativo
+    echo "Numéro informado: $num" . "\nO triplo desse número é: " . $num * 3;
+
+echo "\n";
+
+
+
+
+
